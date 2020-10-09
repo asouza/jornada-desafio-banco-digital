1ceb1656c7a936f8fc0c2ecace31d947bde82c04
alberto souza <alots.ssa@gmail.com>
2020-10-09 07:26:59 -0300

    [nova proposta passo 1] usando teste parametrizado para verificar limites

--- src/test/java/com/deveficiente/bancodigital/novaproposta/AgeValidatorTest.java
+++ src/test/java/com/deveficiente/bancodigital/novaproposta/AgeValidatorTest.java
@@ -5,50 +5,25 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 
 public class AgeValidatorTest {
 	
-
-	@Test
 	@DisplayName("deveria validar que a data de nascimento é igual a permitida")
-	void test1() throws Exception {		
+	@ParameterizedTest
+	@CsvSource({
+		"18,true","19,true","17,false","30,true","10,false"
+	})
+	void test1(int idade,boolean resultadoEsperado) throws Exception {		
 		Age age = Mockito.mock(Age.class);
 		Mockito.when(age.greaterThan()).thenReturn(18);
 		AgeValidator ageValidator = new AgeValidator();
 		ageValidator.initialize(age);
 		
-		LocalDate dezoitoAnos = LocalDate.now().minusYears(18);
+		LocalDate dezoitoAnos = LocalDate.now().minusYears(idade);
 		boolean valido = ageValidator.isValid(dezoitoAnos, null);
-		
-		Assertions.assertTrue(valido);
-	}
-	
-	@Test
-	@DisplayName("deveria validar que a data de nascimento é logo acima da permitida")
-	void test2() throws Exception {		
-		Age age = Mockito.mock(Age.class);
-		Mockito.when(age.greaterThan()).thenReturn(18);
-		AgeValidator ageValidator = new AgeValidator();
-		ageValidator.initialize(age);
-		
-		LocalDate dezoitoAnos = LocalDate.now().minusYears(19);
-		boolean valido = ageValidator.isValid(dezoitoAnos, null);
-		
-		Assertions.assertTrue(valido);
-	}
-	
-	@Test
-	@DisplayName("deveria bloquear data de nascimento é logo abaixo da permitida")
-	void test3() throws Exception {		
-		Age age = Mockito.mock(Age.class);
-		Mockito.when(age.greaterThan()).thenReturn(18);
-		AgeValidator ageValidator = new AgeValidator();
-		ageValidator.initialize(age);
-		
-		LocalDate dezoitoAnos = LocalDate.now().minusYears(17);
-		boolean valido = ageValidator.isValid(dezoitoAnos, null);
-		
-		Assertions.assertFalse(valido);
+		Assertions.assertEquals(resultadoEsperado,valido);
 	}
 }
