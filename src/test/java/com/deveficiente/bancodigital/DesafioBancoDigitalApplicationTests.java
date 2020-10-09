8a71ff9bbe6f70da29c9c7d8a5a3663f7c93a125
alberto souza <alots.ssa@gmail.com>
2020-10-09 06:05:01 -0300

    comeco

---
+++ src/test/java/com/deveficiente/bancodigital/DesafioBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
