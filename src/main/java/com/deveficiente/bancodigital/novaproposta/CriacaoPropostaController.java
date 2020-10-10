f3d8e90814f4a1cc3a62ae31fb1cd9b7042f7999
alberto souza <alots.ssa@gmail.com>
2020-10-10 17:07:43 -0300

    [nova proposta analise complexidade] 14 pontos

--- src/main/java/com/deveficiente/bancodigital/novaproposta/CriacaoPropostaController.java
+++ src/main/java/com/deveficiente/bancodigital/novaproposta/CriacaoPropostaController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+//14 -> Dividir em 2 classes
 public class CriacaoPropostaController {
 
 	/**
@@ -24,12 +25,16 @@ public class CriacaoPropostaController {
 	 */
 
 	@Autowired
+	//1
 	private NovaPropostaRepository novaPropostaRepository;
 
 	@PostMapping(value = "/api/nova-proposta/passo-1")
 	@Transactional
+	//1
 	public NovaPropostaResponse passo1(
+			//1
 			@RequestBody @Valid NovaPropostaPasso1Request request) {
+		//1
 		NovaProposta novaProposta = request.criaNovaProposta();
 		novaPropostaRepository.save(novaProposta);
 
@@ -40,12 +45,15 @@ public class CriacaoPropostaController {
 	@PostMapping(value = "/api/nova-proposta/{codigo}/passo-2")
 	@Transactional
 	public NovaPropostaResponse passo2(@PathVariable("codigo") String codigo,
+			//1
 			@RequestBody @Valid NovaPropostaPasso2Request request) {
 		Optional<NovaProposta> possivelProposta = novaPropostaRepository
 				.findByCodigo(codigo);
+		//1
 		NovaProposta propostaEmAndamento = possivelProposta.orElseThrow(
 				() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
+		//1 NovaPropostaResidencia
 		propostaEmAndamento.atualizaPasso2(request.criaResidencia());
 
 		return new NovaPropostaResponse(propostaEmAndamento,
@@ -56,13 +64,16 @@ public class CriacaoPropostaController {
 	@Transactional
 	public NovaPropostaResponse passo3(
 			@PathVariable("codigo") String codigo,
+			//1
 			@RequestBody @Valid NovaPropostaPasso3Request request) {
 		Optional<NovaProposta> possivelProposta = novaPropostaRepository
 				.findByCodigo(codigo);
 		
+		//1
 		NovaProposta propostaEmAndamento = possivelProposta.orElseThrow(
 				() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
+		//1
 		propostaEmAndamento.getResidencia().orElseThrow(() -> new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY));
 		
 		propostaEmAndamento.atualizaPasso3(request.getLinkFrenteCpf());
@@ -71,11 +82,13 @@ public class CriacaoPropostaController {
 	}
 
 	@GetMapping(value = "/api/nova-proposta/{codigo}")
+	//1
 	public DetalhePropostaResponse detalhe(
 			@PathVariable("codigo") String codigo) {
 		Optional<NovaProposta> possivelProposta = novaPropostaRepository
 				.findByCodigo(codigo);
 
+		//1
 		return possivelProposta.map(DetalhePropostaResponse::new).orElseThrow(
 				() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
@@ -87,8 +100,10 @@ public class CriacaoPropostaController {
 		Optional<NovaProposta> possivelProposta = novaPropostaRepository
 				.findByCodigo(codigo);
 
+		//1
 		NovaProposta proposta = possivelProposta.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 		
+		//1
 		if(!proposta.completa()) {
 			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
 		}
